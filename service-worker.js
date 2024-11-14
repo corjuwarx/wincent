dfc1b8ab577f94a3fddcbd58124cc2a2101e5d75
corjuwarx <119467729+corjuwarx@users.noreply.github.com>
2024-11-14 03:09:36 +0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,11 @@
 self.addEventListener('install', (event) => {
-    console.log('Service Worker yüklendi');
-    event.waitUntil(
-        caches.open('wincent-cache').then((cache) => {
-            return cache.addAll([
-                '/', 
-                '/index.html',
-                '/style.css',
-                '/images/favicon.jpg',
-            ]);
-        })
-    );
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
-        })
-    );
+  event.respondWith(fetch(event.request));
 });
